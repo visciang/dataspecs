3b5502a7610f2117767523ce8039c11328d4565a
Giovanni Visciano <giovanni.visciano@athonet.com>
2021-07-14 14:25:56 +0200

    refactoring

--- lib/dataspec/loaders.ex
+++ lib/dataspec/loaders.ex
@@ -1,106 +1,106 @@
 defmodule DataSpec.Loaders do
   alias DataSpec.Error
 
-  def literal(literal, value, __type_params_processors) do
+  def literal(literal, value, _type_params_loaders) do
     case value do
       ^literal -> value
       _ -> raise Error, "can't convert #{inspect(value)} to literal #{inspect(literal)}"
     end
   end
 
-  def any(value, __type_params_processors) do
+  def any(value, _type_params_loaders) do
     value
   end
 
-  def atom(value, __type_params_processors) do
+  def atom(value, _type_params_loaders) do
     case value do
       value when is_atom(value) -> value
       _ -> raise Error, "can't convert #{inspect(value)} to an atom"
     end
   end
 
-  def boolean(value, __type_params_processors) do
+  def boolean(value, _type_params_loaders) do
     case value do
       value when is_boolean(value) -> value
       _ -> raise Error, "can't convert #{inspect(value)} to a boolean"
     end
   end
 
-  def binary(value, __type_params_processors) do
+  def binary(value, _type_params_loaders) do
     case value do
       value when is_binary(value) -> value
       _ -> raise Error, "can't convert #{inspect(value)} to a binary"
     end
   end
 
-  def pid(value, __type_params_processors) do
+  def pid(value, _type_params_loaders) do
     case value do
       value when is_pid(value) -> value
       _ -> raise Error, "can't convert #{inspect(value)} to a pid"
     end
   end
 
-  def reference(value, __type_params_processors) do
+  def reference(value, _type_params_loaders) do
     case value do
       value when is_reference(value) -> value
       _ -> raise Error, "can't convert #{inspect(value)} to a reference"
     end
   end
 
-  def number(value, __type_params_processors) do
+  def number(value, _type_params_loaders) do
     case value do
       value when is_number(value) -> value
       _ -> raise Error, "can't convert #{inspect(value)} to a number"
     end
   end
 
-  def float(value, __type_params_processors) do
+  def float(value, _type_params_loaders) do
     case value do
       value when is_number(value) -> :erlang.float(value)
       _ -> raise Error, "can't convert #{inspect(value)} to a float"
     end
   end
 
-  def integer(value, __type_params_processors) do
+  def integer(value, _type_params_loaders) do
     case value do
       value when is_integer(value) -> value
       _ -> raise Error, "can't convert #{inspect(value)} to an integer"
     end
   end
 
-  def neg_integer(value, __type_params_processors) do
+  def neg_integer(value, _type_params_loaders) do
     case value do
       value when is_integer(value) and value < 0 -> value
       _ -> raise Error, "can't convert #{inspect(value)} to a neg_integer"
     end
   end
 
-  def non_neg_integer(value, __type_params_processors) do
+  def non_neg_integer(value, _type_params_loaders) do
     case value do
       value when is_integer(value) and value >= 0 -> value
       _ -> raise Error, "can't convert #{inspect(value)} to a non_neg_integer"
     end
   end
 
-  def pos_integer(value, __type_params_processors) do
+  def pos_integer(value, _type_params_loaders) do
     case value do
       value when is_integer(value) and value > 0 -> value
       _ -> raise Error, "can't convert #{inspect(value)} to a pos_integer"
     end
   end
 
-  def range(lower, upper, value, __type_params_processors) do
+  def range(lower, upper, value, _type_params_loaders) do
     case value do
       value when is_integer(value) and lower <= value and value <= upper -> value
       _ -> raise Error, "can't convert #{inspect(value)} to a range of #{inspect(lower..upper)}"
     end
   end
 
-  def union(value, type_params_processors) do
-    type_params_processors
-    |> Enum.reduce_while(:error, fn processor, _ ->
+  def union(value, type_params_loaders) do
+    type_params_loaders
+    |> Enum.reduce_while(:error, fn loader, _ ->
       try do
-        {:halt, {:ok, processor.(value, [])}}
+        {:halt, {:ok, loader.(value, [])}}
       rescue
         Error ->
           {:cont, :error}
@@ -112,23 +112,23 @@ defmodule DataSpec.Loaders do
     end
   end
 
-  def empty_list(value, _type_params_processors) do
+  def empty_list(value, _type_params_loaders) do
     case value do
       [] -> value
       _ -> raise Error, "can't convert #{inspect(value)} to an empty list"
     end
   end
 
-  def nonempty_list(value, _type_params_processors) do
+  def nonempty_list(value, _type_params_loaders) do
     case value do
       [_ | _] -> value
       _ -> raise Error, "can't convert #{inspect(value)} to a non empty list"
     end
   end
 
-  def list(value, [type_params_processor]) do
+  def list(value, [type_params_loader]) do
     case value do
-      value when is_list(value) -> Enum.map(value, &type_params_processor.(&1, []))
+      value when is_list(value) -> Enum.map(value, &type_params_loader.(&1, []))
       _ -> raise Error, "can't convert #{inspect(value)} to a list"
     end
   end
@@ -141,8 +141,8 @@ defmodule DataSpec.Loaders do
     end
   end
 
-  def map_field_required(map, [type_key_processor, type_value_processor]) do
-    {map_rest, map_processed} = map_field_optional(map, [type_key_processor, type_value_processor])
+  def map_field_required(map, [type_key_loader, type_value_loader]) do
+    {map_rest, map_processed} = map_field_optional(map, [type_key_loader, type_value_loader])
 
     if map_size(map_processed) == 0 do
       raise Error, "can't convert #{inspect(map)} to a map, missing required k/v"
@@ -151,13 +151,13 @@ defmodule DataSpec.Loaders do
     {map_rest, map_processed}
   end
 
-  def map_field_optional(map, [type_key_processor, type_value_processor]) do
+  def map_field_optional(map, [type_key_loader, type_value_loader]) do
     case map do
       map when is_map(map) ->
         Enum.reduce(map, {map, %{}}, fn {map_key, map_value}, {map_rest, map_processed} ->
           try do
-            map_key_processed = type_key_processor.(map_key, [])
-            map_value_processed = type_value_processor.(map_value, [])
+            map_key_processed = type_key_loader.(map_key, [])
+            map_value_processed = type_value_loader.(map_value, [])
             map_processed = Map.put(map_processed, map_key_processed, map_value_processed)
 
             map_rest = Map.delete(map_rest, map_key)
@@ -174,13 +174,13 @@ defmodule DataSpec.Loaders do
     end
   end
 
-  def tuple(value, type_params_processors) do
+  def tuple(value, type_params_loaders) do
     case value do
       value when is_tuple(value) ->
         value
         |> Tuple.to_list()
-        |> Enum.zip(type_params_processors)
-        |> Enum.map(fn {item, processor} -> processor.(item, []) end)
+        |> Enum.zip(type_params_loaders)
+        |> Enum.map(fn {item, loader} -> loader.(item, []) end)
         |> List.to_tuple()
 
       _ ->
