b69a666d45fb5d97898d1024daad09f44342539c
Giovanni Visciano <giovanni.visciano@athonet.com>
2021-08-02 19:44:56 +0200

    update and minor fix

--- mix.exs
+++ mix.exs
@@ -39,7 +39,6 @@ defmodule DataSpecs.Mixfile do
     ]
   end
 
-  defp elixirc_paths(:ci), do: ["lib", "test/support"]
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
