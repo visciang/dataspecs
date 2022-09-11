f134ac3b2b5c810b808fafce412881c408a028fe
Giovanni Visciano <giovanni.visciano@athonet.com>
2022-09-11 21:01:04 +0200

    colors

--- lib/mix/tasks/dataspecs.schema.dump.ex
+++ lib/mix/tasks/dataspecs.schema.dump.ex
@@ -16,7 +16,7 @@ defmodule Mix.Tasks.Dataspecs.Schema.Dump do
     "Elixir.#{module}"
     |> String.to_atom()
     |> DataSpecs.Schema.load()
-    |> inspect(pretty: true, limit: :infinity)
+    |> inspect(pretty: true, syntax_colors: IO.ANSI.syntax_colors, limit: :infinity)
     |> Mix.shell().info()
   end
 end
